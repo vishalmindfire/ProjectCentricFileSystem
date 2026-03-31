2e8087fd5dfed8b7723c9967759e6e57129c455b
Vishal Minz <vishalm@mindfiresolutions.com>
2026-03-31 17:23:09 +0530

    login handler changes

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -36,32 +36,14 @@ function LoginForm(){
            navigate('/projects', { replace: true });
         }
     }
-
-    const onEmailChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(event.target.value){
-            setEmailError(null);
-        }else{
-            setEmailError("test error");
-        }
-        console.log("Input value changed:", event.target.value);
-    }
-
-    const onPasswordChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(event.target.value){
-            setPasswordError("test error");
-        }else{
-            setPasswordError(null);
-        }
-        console.log("Input value changed:", event.target.value);
-    }
   return (
     <form onSubmit={logIn} className="login-form">
         <div className='login-header'>
             <h1>Login</h1>
         </div>
         <div className='login-body'>
-            <InputBox label="Email" name="email" id="email" type="text" onChange={onEmailChangeEventHandler} error={emailError} data-testid="email-input" />
-            <InputBox label="Password" name="password" id="password" type="password" onChange={onPasswordChangeEventHandler} error={passwordError} data-testid="password-input" />
+            <InputBox label="Email" name="email" id="email" type="text"  error={emailError} data-testid="email-input" />
+            <InputBox label="Password" name="password" id="password" type="password" error={passwordError} data-testid="password-input" />
         </div>
         <InputBox name="login" id="login" value="Log In" type="submit"/>
     </form>
