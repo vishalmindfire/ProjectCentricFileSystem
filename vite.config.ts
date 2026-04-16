da2c26d0e7c18693da7cd89c2e043d704e34a105
Vishal Minz <vishalm@mindfiresolutions.com>
2026-04-16 06:06:08 +0530

    deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path/win32';
-import mkcert from 'vite-plugin-mkcert';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, 'src/styles'),
-      '@': path.resolve(__dirname, 'src'),
     },
     tsconfigPaths: true,
   },
